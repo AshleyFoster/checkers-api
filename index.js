ee43e947484437f19a93454ff191c701b3cdcd00
Ashley Foster <ashley@ashleyfoster.me>
2018-01-10 11:00:04 -0500

    This fixes connection issues

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ import socketIO from 'socket.io'
 
 const app = express()
 const server = http.Server(app)
-const io = socketIO(http)
+const io = socketIO(server)
+
+server.listen(3001, function(){
+  console.log('listening on *:3001');
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+io.on('connection', function(socket) {
 
-server.listen(3001, function(){
-  console.log('listening on *:3001');
+  console.log('a user connected');
+
+  // socket.on('disconnect', function(){
+  //   console.log('user disconnected');
+  // });
 });
